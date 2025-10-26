f2b4d4379e7833e64ff1078e71316f35ec752dfc
masaevke <kirill.masaev@yandex.ru>
2025-10-26 18:04:12 +0500

    add CONFIG_PERSON and CONFIG_ORG

--- laureates_configs.py
+++ laureates_configs.py
@@ -11,3 +11,32 @@ def process_year(year_string: str) -> Optional[int]:
         return int(year_string[:4])
     except ValueError:
         return None
+
+
+CONFIG_PERSON = {
+    "id": ["id"],
+    "name": ["knownName", "en"],
+    "gender": ["gender"],
+    "birth_year": (["birth", "date"], process_year),
+    "country_birth": ["birth", "place", "country", "en"],
+    "country_now": ["birth", "place", "countryNow", "en"],
+    #обрабатывается процессором призов, который вы задаете в конфиге для призов
+    "prizes_relevant": (["nobelPrizes"], lambda prizes: prizes) 
+}
+
+CONFIG_ORG = {
+    "id": ["id"],
+    "name": ["orgName", "en"],
+    "founded_year": (["founded", "date"], process_year),
+    "country_founded": ["founded", "place", "country", "en"],
+    "country_now": ["founded", "place", "countryNow", "en"],
+    #обрабатывается процессором призов, который вы задаете в конфиге для призов
+    "prizes_relevant": (["nobelPrizes"], lambda prizes: prizes)  
+}
+
+
+def person_processor():
+    return create_processor(CONFIG_PERSON, list_processor=False)
+
+def org_processor():
+    return create_processor(CONFIG_ORG, list_processor=False)
