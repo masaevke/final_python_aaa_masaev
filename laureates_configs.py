15f1324a8c23ac47b56770ef0bacb0ae932561bb
masaevke <kirill.masaev@yandex.ru>
2025-10-26 17:57:26 +0500

    add process_year

--- laureates_configs.py
+++ laureates_configs.py
@@ -1,1 +1,13 @@
+from json_dict_processing import create_processor
+from typing import Optional
 
+
+def process_year(year_string: str) -> Optional[int]:
+    """вспомогательная -
+    извлекает год из строки в формате 'YYYY-MM-DD'."""
+    if not year_string or not isinstance(year_string, str):
+        return None
+    try:
+        return int(year_string[:4])
+    except ValueError:
+        return None
